0496ebe0cc119b1f769ea6381e9d29c0ec71595c
David McCown <dmccown@chef.io>
2018-11-08 15:36:21 -0600

    Introduces a service method
    Adding a new service method where you can look up a service by origin
    and name.
    
    Signed-off-by: David McCown <dmccown@chef.io>

--- lib/train-habitat/httpgateway.rb
+++ lib/train-habitat/httpgateway.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative 'illegal_state_error'
 
 require 'net/http'
 require 'json'
@@ -14,6 +15,17 @@ module TrainPlugins
 
       def services
         JSON.parse(Net::HTTP.get_response(uri).body)
+      end
+
+      def service(origin, name)
+        service = services.select do |svc|
+          svc.dig('pkg', 'origin') == origin && svc.dig('pkg', 'name') == name
+        end
+
+        raise IllegalStateError.new("Expected one service '#{origin}/#{name}', but found none.") if service.empty?
+        raise IllegalStateError.new("Expected one service '#{origin}/#{name}', but found multiple.") if service.size > 1
+
+        service.first
       end
     end
   end
---
+++ lib/train-habitat/illegal_state_error.rb
@@ -0,0 +1,5 @@
+class IllegalStateError < StandardError
+  def initialize(msg=nil)
+    super
+  end
+end
