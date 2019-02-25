18e750ed6dca3233af518613bc8394b99f2b3dec
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-02-25 12:53:55 -0500

    Unit tests passing for option detect for CLI/API mode
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/train-habitat/connection.rb
+++ lib/train-habitat/connection.rb
@@ -4,21 +4,23 @@ require 'net/http'
 require 'json'
 require 'train-habitat/httpgateway'
 require 'train-habitat/platform'
+require 'train-habitat/transport'
 
 module TrainPlugins
   module Habitat
     class Connection < Train::Plugins::Transport::BaseConnection
       include TrainPlugins::Habitat::Platform
 
+      attr_reader :cli_transport_name, :cli_connection, :transport_options
+
       def initialize(options = {})
-        if options.nil? || options[:host].nil?
-          raise Train::TransportError, 'Habitat HTTP Gateway host required'
+        @transport_options = options
+
+        unless api_options_provided? || cli_options_provided?
+          raise Train::TransportError, "Habitat connection options that begin with either 'cli' or 'api' required"
         end
 
-        super(options)
-        @cache_enabled[:api_call] = true
-        @cache[:api_call]         = {}
-
+        super(transport_options)
         enable_cache :api_call
       end
 
@@ -26,10 +28,65 @@ module TrainPlugins
         "habitat://#{@options[:host]}"
       end
 
-      def habitat_client
-        cached_client(:api_call, :HTTPGateway) do
-          HTTPGateway.new(@options[:host])
+      def api_options_provided?
+        have_transport_options_with_prefix?('api')
+      end
+
+      def cli_options_provided?
+        TrainPlugins::Habitat::Transport.cli_transport_prefixes.keys.map(&:to_s).any? do |prefix|
+          have_transport_options_with_prefix?(prefix)
         end
+      end
+
+      # Use this to execuute a `hab` command. Do not include the `hab` executable in the invocation.
+      def run_hab_cli(command, exec_options = {})
+        raise CliNotAvailableError(cli_tranport_names) unless cli_options_provided?
+        initialize_cli_connection!
+        # TODO - leverage exec_options to add things like JSON parsing, ENV setting, etc.
+        cli_connection.run_command(hab_path + ' ' + command)
+      end
+
+      def habitat_api_client
+        cached_client(:api_call, :HTTPGateway) do
+          # TODO: add authtoken
+          HTTPGateway.new(@options[:api_host])
+        end
+      end
+
+      private
+
+      def cli_transport_names
+        TrainPlugins::Habitat::Transport.cli_transport_prefixes.values
+      end
+
+      def have_transport_options_with_prefix?(prefix)
+        transport_options.keys.map(&:to_s).any? { |option_name| option_name.start_with? prefix }
+      end
+
+      def initialize_cli_connection!
+        return if @cli_connection
+        raise CliNotAvailableError(cli_tranport_names) unless cli_options_provided?
+
+        # group cli connection options by prefix
+        # Our incoming connection options have prefixes, so we see things like 'cli_ssh_host'.
+        # The ssh transport wants just 'host'. So identify which transports we have
+        # options for, and trim them down.
+        known_prefixes = TrainPlugins::Habitat::Transport.cli_transport_prefixes.keys.map(&:to_s)
+        seen_cli_transports = transport_options.each_with_object({}) do |xport_option_name, xport_option_value, seen_xports|
+          known_prefixes.each do |prefix|
+            if xport_option_name.to_s.start_with(prefix)
+              xport_name = TrainPlugins::Habitat::Transport.cli_transport_prefixes[prefix]
+              seen_xports[xport_name] ||= {}
+              # Remove the prefix from the option and store under transport name
+              seen_xports[xport_name][xport_option_name.to_s.sub(/^#{prefix}_/,'').to_sym] = xport_option_value
+            end
+          end
+        end
+
+        raise MultipleCliTransportsError.new(seen_cli_tranports.keys) if seen_cli_transports.count > 1
+
+        @cli_transport_name = seen_cli_tranports.keys.first
+        @cli_connection = Train.create(cli_transport_name, seen_cli_tranports[cli_tranpoort_name])
       end
     end
   end
--- test/unit/connection_test.rb
+++ test/unit/connection_test.rb
@@ -7,7 +7,7 @@ require './lib/train-habitat/connection'
 describe TrainPlugins::Habitat::Connection do
   subject { TrainPlugins::Habitat::Connection }
 
-  let(:opt)   { { host: 'habitat01.inspec.io' } }
+  let(:opt)   { { api_host: 'habitat01.inspec.io' } }
   let(:conn)  { subject.new(opt) }
   let(:cache) { conn.instance_variable_get(:@cache) }
 
@@ -26,10 +26,6 @@ describe TrainPlugins::Habitat::Connection do
         subject.instance_methods(false).wont_include(method_name)
       end
     end
-
-    it 'should declare itself as a non-local transport' do
-      subject.new(opt).local?.must_equal(false)
-    end
   end
 
   describe '#new' do
@@ -40,21 +36,67 @@ describe TrainPlugins::Habitat::Connection do
     end
   end
 
-  describe '#uri' do
-    it 'should have the correct habitat uri' do
-      conn.uri.must_equal "habitat://#{opt[:host]}"
+  describe '#api_options_provided?' do
+    describe 'when api options are present' do
+      let(:opt) { { api_url: 'http://somewhere.com' } }
+      it 'should find the options' do
+        conn.api_options_provided?.must_equal true
+      end
+    end
+    describe 'when api options are absent' do
+      let(:opt) { { cli_test_host: 'somewhere.com'} }
+      it 'should not find the options' do
+        mock_ctp = Minitest::Mock.new
+        mock_ctp.expect(:call, { cli_test: :test })
+        TrainPlugins::Habitat::Transport.stub(:cli_transport_prefixes, mock_ctp) do
+          conn.api_options_provided?.must_equal false
+        end
+      end
     end
   end
 
-  describe '#habitat_client' do
+  describe '#cli_options_provided?' do
+    describe 'when cli options are present' do
+      let(:opt) { { cli_test_host: 'somewhere.com'} }
+      it 'should find the options' do
+        mock_ctp = Minitest::Mock.new
+        mock_ctp.expect(:call, { cli_test: :test })
+        mock_ctp.expect(:call, { cli_test: :test })
+        TrainPlugins::Habitat::Transport.stub(:cli_transport_prefixes, mock_ctp) do
+          conn.cli_options_provided?.must_equal true
+        end
+      end
+    end
+
+    describe 'when cli options are absent' do
+      let(:opt) { { api_host: 'somewhere.com'} }
+      it 'should not find the options' do
+        mock_ctp = Minitest::Mock.new
+        mock_ctp.expect(:call, { cli_test: :test })
+        TrainPlugins::Habitat::Transport.stub(:cli_transport_prefixes, mock_ctp) do
+          conn.cli_options_provided?.must_equal false
+        end
+      end
+    end
+  end
+
+  describe 'when neither cli nor api options are present' do
+    let(:opt) { { host: 'somewhere.com'} }
+    it 'should not find the options' do
+      assert_raises(Train::TransportError) { conn }
+    end
+  end
+
+
+  describe '#habitat_api_client' do
     it 'should return kind of TrainPlugins::Habitat::HTTPGateway' do
-      conn.habitat_client.must_be_kind_of TrainPlugins::Habitat::HTTPGateway
+      conn.habitat_api_client.must_be_kind_of TrainPlugins::Habitat::HTTPGateway
     end
 
     it 'returns a new instance when cache is disabled' do
       conn.disable_cache(:api_call)
-      client_one = conn.habitat_client
-      client_two = conn.habitat_client
+      client_one = conn.habitat_api_client
+      client_two = conn.habitat_api_client
 
       cache[:api_call].count.must_equal 0
       client_one.object_id.wont_equal client_two.object_id
@@ -64,8 +106,8 @@ describe TrainPlugins::Habitat::Connection do
       cache[:api_call].count.must_equal 0
       conn.enable_cache(:api_call)
 
-      client_one = conn.habitat_client
-      client_two = conn.habitat_client
+      client_one = conn.habitat_api_client
+      client_two = conn.habitat_api_client
 
       cache[:api_call].count.must_equal 1
       client_one.object_id.must_equal client_two.object_id
