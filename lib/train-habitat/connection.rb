866e71180e9ee8bf79c887b5d1fcbc478752aa1b
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-03-12 14:47:21 -0400

    Fix CLI option parsing
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/train-habitat/connection.rb
+++ lib/train-habitat/connection.rb
@@ -15,10 +15,10 @@ module TrainPlugins
 
       def initialize(options = {})
         @transport_options = options
-
         validate_transport_options!
 
         super(transport_options)
+        initialize_cli_connection! if cli_options_provided?
         enable_cache :api_call
       end
 
@@ -39,7 +39,6 @@ module TrainPlugins
       # Use this to execute a `hab` command. Do not include the `hab` executable in the invocation.
       def run_hab_cli(command, exec_options = {})
         raise CliNotAvailableError(cli_tranport_names) unless cli_options_provided?
-        initialize_cli_connection!
         # TODO - leverage exec_options to add things like JSON parsing, ENV setting, etc.
         cli_connection.run_command(hab_path + ' ' + command)
       end
@@ -95,21 +94,24 @@ module TrainPlugins
         # The ssh transport wants just 'host'. So identify which transports we have
         # options for, and trim them down.
         known_prefixes = TrainPlugins::Habitat::Transport.cli_transport_prefixes.keys.map(&:to_s)
-        seen_cli_transports = transport_options.each_with_object({}) do |xport_option_name, xport_option_value, seen_xports|
+
+        seen_cli_transports = {}
+        transport_options.each do |xport_option_name, xport_option_value|
           known_prefixes.each do |prefix|
-            if xport_option_name.to_s.start_with(prefix)
-              xport_name = TrainPlugins::Habitat::Transport.cli_transport_prefixes[prefix]
-              seen_xports[xport_name] ||= {}
+            if xport_option_name.to_s.start_with?(prefix)
+              xport_name = TrainPlugins::Habitat::Transport.cli_transport_prefixes[prefix.to_sym]
+
+              seen_cli_transports[xport_name] ||= {}
               # Remove the prefix from the option and store under transport name
-              seen_xports[xport_name][xport_option_name.to_s.sub(/^#{prefix}_/,'').to_sym] = xport_option_value
+              seen_cli_transports[xport_name][xport_option_name.to_s.sub(/^#{prefix}_/,'').to_sym] = xport_option_value
             end
           end
         end
 
         raise MultipleCliTransportsError.new(seen_cli_tranports.keys) if seen_cli_transports.count > 1
 
-        @cli_transport_name = seen_cli_tranports.keys.first
-        @cli_connection = Train.create(cli_transport_name, seen_cli_tranports[cli_tranpoort_name])
+        @cli_transport_name = seen_cli_transports.keys.first
+        @cli_connection = Train.create(cli_transport_name, seen_cli_transports[cli_transport_name])
       end
     end
   end
