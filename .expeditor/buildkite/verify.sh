079debbed252cfe919df3442e3c3d51e6ff06546
Tim Smith <tsmith84@gmail.com>
2020-09-02 10:21:35 -0700

    Cache gem installs to speed up tests
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -8,6 +8,7 @@ ruby -v
 bundle --version
 
 echo "--- bundle install"
+bundle config --local path vendor/bundle
 bundle install --jobs=7 --retry=3 --without tools maintenance deploy
 
 echo "+++ bundle exec rake lint"
